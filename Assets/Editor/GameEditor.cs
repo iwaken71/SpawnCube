38f4c054cc79a1b4cffc94f26218c93f70ce0798
岩崎謙汰 <iwaken59371518@gmail.com>
2016-03-09 15:39:38 +0900

    最初

---
+++ Assets/Editor/GameEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(GameScript))]
+public class GameEditor : Editor {
+	
+
+	public override void OnInspectorGUI(){
+
+		DrawDefaultInspector ();
+		GameScript myScript = (GameScript)target;
+		//EditorGUILayout.IntField("x",)
+
+
+		if (GUILayout.Button ("Spawnx")) {
+			myScript.Spawnx ();
+
+		}
+			
+
+
+		//myScript.pos = new Vector3 (x,y,z);
+
+
+	}
+
+
+
+}
---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameScript : MonoBehaviour {
+
+	public Vector3 pos = Vector3.zero;
+	public GameObject cube;
+
+	public int x = 0, z = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Spawn(){
+		Instantiate (cube, pos, Quaternion.identity);
+	}
+
+	public void Spawnx(){
+		GameObject obj = new GameObject ("1");
+		for(int i = 0 ;i < z; i++){
+			for(int j = 0; j < x; j++){
+				
+				GameObject obj2 =  Instantiate(cube,pos + new Vector3(j,0,i),Quaternion.identity) as GameObject;
+				obj2.transform.parent = obj.transform;
+
+			}
+
+		}
+
+	}
+}
